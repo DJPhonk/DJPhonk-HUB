3f1b08387e4ae01273b2dde6586cbef6c914cc7a
DJPhonk <hakersenos57@gmail.com>
2025-04-29 10:29:51 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,7 +1,7 @@
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
+
 game:GetService("StarterGui"):SetCore("SendNotification", { 
 	Title = "DJ Phonk";
 	Text = "BY DJ Phonk";
 	Icon = "https://create.roblox.com/store/asset/106808374231896/djphonk"})
 Duration = 5;
-
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
