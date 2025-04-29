eff7273528974b367185cea53a52557153646f77
DJPhonk <hakersenos57@gmail.com>
2025-04-29 13:27:01 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -3,5 +3,5 @@ game:GetService("StarterGui"):SetCore("SendNotification", {
     Text = "BY DJ Phonk";
     Icon = "rbxthumb://type=Asset&id=106808374231896&w=150&h=150";
     Duration = 5;
-})
+}) 
 return loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
