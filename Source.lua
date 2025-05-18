3741c0988363b1dcba66ce6a055e504e6c772722
DJPhonk <hakersenos57@gmail.com>
2025-05-18 19:10:28 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -4,4 +4,4 @@ game:GetService("StarterGui"):SetCore("SendNotification", {
     Icon = "rbxthumb://type=Asset&id=106808374231896&w=150&h=150";
     Duration = 5;
 }) 
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings
